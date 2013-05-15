af0258cc3b83bfa382da4534490d4cb560252c01
otavioarj <otavioarj@gmail.com>
2013-05-15 02:21:25 -0300

    First commit

--- README.md
+++ README.md
@@ -2,3 +2,25 @@ gbucket
 =======
 
 Grimly Bucket Sort
+
+A Python implemented Bucket Sort, where worst scenario for sorting A[1...n] is Theta(n). That is possible by using a hashing function to index elements on buckets.
+Every element is placed at tail of Bucket_list[x], for x=e*i mod (size Bucket_list), where e is A[i].
+
+That code have the worst scenario for regular Bucket Sort already implemented. It is the default execution:
+$ python gbucket.py
+10
+20
+0
+20
+0
+20
+0
+20
+0
+20
+0
+[65536, 65536, 65536, ..... 65536]
+
+Where each line before A print is actually the size of each Bucket at Bucket_list. So for 100 elements equals 2^16, that algorithm use at maximum 20 buckets in one list. 
+If any parameter is given, /dev/urandom is read for 100 pseudo-aleatory elements.
+
---
+++ gbucket.py
@@ -0,0 +1,69 @@
+# Author Otavio Augusto, at otavioarj gmail.com
+# This code is at GPLv2
+
+#!/usr/bin/python
+import struct, math,sys
+
+
+def bucketsort( A):
+	code = hashing(len(A))
+	buckets = [list() for _ in range( code )]
+#	print code[1]
+	for i in range(0,len(A),1):		
+		x = re_hashing(A[i], i, len(buckets))
+		buckets[x].append( A[i] )
+	print len(buckets)
+	for x  in range(0,len( buckets)):
+		print len(buckets[x])
+		buckets[x]=merge_sort(buckets[x])
+	ndx=0
+	for b in range( len( buckets ) ):
+		for v in buckets[b]:
+			A[ndx] = v
+			ndx += 1
+ 
+ 
+def hashing( lA):
+	return int( math.sqrt(lA))
+ 
+ 
+def re_hashing(x, i, bucks  ):
+  return int((i*x) % bucks) 
+
+def merge_sort(li):
+    if len(li) < 2: return li
+    m = len(li) / 2
+    return merge(merge_sort(li[:m]), merge_sort(li[m:]))
+
+def merge(l, r):
+    result = []
+    i = j = 0
+    while i < len(l) and j < len(r):
+        if l[i] < r[j]:
+            result.append(l[i])
+            i += 1
+        else:
+            result.append(r[j])
+            j += 1            
+    result += l[i:]
+    result += r[j:]
+    return result
+
+
+def main():
+	A=[]
+	file=open("/dev/urandom","rb")
+	for i in range(0,100):
+		if(len(sys.argv)>1):
+			barray=file.read(4)
+			num=0
+			for x in range(4,-1,-1):
+				num+=ord(barray[x-1])*8*(4-x)
+		else:
+			num=2**16
+		A.append(num)
+	#A=[2,5,7,9]
+	bucketsort(A)
+	print A
+
+main()
